18491c4b6e705c9998e92dbaaa1dbb8459a42828
Jackson Liu <416883813@qq.com>
2016-08-12 02:26:08 +0800

    add main run to get data

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -1,21 +1,75 @@
-//创建根实例
-// let content =  new Vue ({
-//     el: '#content'
-// });
+const rankNumber = 15;
+
+let parseLive = live => {
+        let nums = live.nums;
+        live.nums = nums > 10000 ? (nums/10000).toFixed(1) + "万" : nums;
+};
+
+let getRankinfo = (liveinfos) => {
+    let lives = [],
+        orderedlives = [];
+
+    //取出所有直播信息拼成大数组
+    liveinfos.forEach( (item, i) => {
+        lives = lives.concat(item.lives);
+    });
 
 
-var websites = new Vue ({
+    orderedlives = lives.sort( (pre, after) => {
+        return after.nums - pre.nums;
+    });
+    orderedlives = orderedlives.slice(0, rankNumber);
+    orderedlives.forEach( live => {
+        parseLive(live);
+    });
+
+    return orderedlives;
+};
+
+
+let getliveinfos = () => {
+    return new Promise((resolve, reject) => {
+        fetch("/search", {
+            method: "get",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            }).then((res) => {
+            if (res.ok) {
+                res.json().then(data => {
+                    resolve(data);
+                })
+            }
+        });
+    })
+};
+
+let globaldata = {
+    liveinfos: {},
+    ranklives: [],
+    siteindex: 0
+}
+
+let websites = new Vue({
     el: "#websites",
     data: {
-        liveinfos: [
-            {website:'熊猫'},
-            {website: '虎牙'}
-        ],
-        checknum: 0
+        liveinfos: [{
+            website: '熊猫'
+        }, {
+            website: '战旗'
+        }, {
+            website: '斗鱼'
+        }, {
+            website: '虎牙'
+        }, {
+            website: '全民'
+        }],
+        checknum: globaldata.siteindex
     },
     methods: {
-        getWebsitelive: function (e) {
-            if(e.target.tagName === 'LI') {
+        getWebsitelive: function(e) {
+            if (e.target.tagName === 'LI') {
                 let ele = e.target;
                 this.checknum = ele.getAttribute('index');
                 console.log(typeof this.checknum);
@@ -23,3 +77,12 @@ var websites = new Vue ({
         }
     }
 });
+
+//获取直播信息
+(function run() {
+    getliveinfos().then( (data) => {
+        globaldata.liveinfos = data;
+        globaldata.ranklives = getRankinfo(data);
+        console.log(data);
+    });
+})();
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,18 +1,73 @@
-'use strict';
+"use strict";
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-//创建根实例
-// let content =  new Vue ({
-//     el: '#content'
-// });
+var rankNumber = 15;
 
+var parseLive = function parseLive(live) {
+    var nums = live.nums;
+    live.nums = nums > 10000 ? (nums / 10000).toFixed(1) + "万" : nums;
+};
+
+var getRankinfo = function getRankinfo(liveinfos) {
+    var lives = [],
+        orderedlives = [];
+
+    //取出所有直播信息拼成大数组
+    liveinfos.forEach(function (item, i) {
+        lives = lives.concat(item.lives);
+    });
+
+    orderedlives = lives.sort(function (pre, after) {
+        return after.nums - pre.nums;
+    });
+    orderedlives = orderedlives.slice(0, rankNumber);
+    orderedlives.forEach(function (live) {
+        parseLive(live);
+    });
+
+    return orderedlives;
+};
+
+var getliveinfos = function getliveinfos() {
+    return new Promise(function (resolve, reject) {
+        fetch("/search", {
+            method: "get",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(function (res) {
+            if (res.ok) {
+                res.json().then(function (data) {
+                    resolve(data);
+                });
+            }
+        });
+    });
+};
+
+var globaldata = {
+    liveinfos: {},
+    ranklives: [],
+    siteindex: 0
+};
 
 var websites = new Vue({
     el: "#websites",
     data: {
-        liveinfos: [{ website: '熊猫' }, { website: '虎牙' }],
-        checknum: 0
+        liveinfos: [{
+            website: '熊猫'
+        }, {
+            website: '战旗'
+        }, {
+            website: '斗鱼'
+        }, {
+            website: '虎牙'
+        }, {
+            website: '全民'
+        }],
+        checknum: globaldata.siteindex
     },
     methods: {
         getWebsitelive: function getWebsitelive(e) {
@@ -24,3 +79,12 @@ var websites = new Vue({
         }
     }
 });
+
+//获取直播信息
+(function run() {
+    getliveinfos().then(function (data) {
+        globaldata.liveinfos = data;
+        globaldata.ranklives = getRankinfo(data);
+        console.log(data);
+    });
+})();
